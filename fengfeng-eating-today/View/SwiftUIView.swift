6315495f523cfaa6e6a537cf5c7995e35b20e826
swanine <haohao1500@foxmail.com>
2024-04-11 21:21:53 +0800

    refactor: update food pictures

---
+++ fengfeng-eating-today/View/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  fengfeng-eating-today
+//
+//  Created by swanine on 2024/3/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
